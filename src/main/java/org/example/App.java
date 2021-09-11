871710da41ecb674127fd9c0a76cbf403e2af1ae
ajdahcode <aj.bagarra@gmail.com>
2021-09-10 22:55:37 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,20 @@
+package org.example;
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner scnN = new Scanner(System.in);
+        System.out.print( "What is the first number? " );
+        String num1 = scnN.next();
+        int number1 = Integer.parseInt(num1);
+        System.out.print( "What is the second number? " );
+        String num2 = scnN.next();
+        int number2 = Integer.parseInt(num2);
+        System.out.println( num1 +" + "+ num2 +" = "+ (number1+number2) );
+        System.out.println( num1 +" - "+ num2 +" = "+ (number1-number2) );
+        System.out.println( num1 +" * "+ num2 +" = "+ (number1*number2) );
+        System.out.print( num1 +" / "+ num2 +" = "+ (number1/number2) );
+    }
+}
